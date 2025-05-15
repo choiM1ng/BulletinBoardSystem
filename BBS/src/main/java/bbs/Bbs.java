4abe30c5ff66b6babf37b252b197cd00fe998d61
choiM1ng <furigia06@naver.com>
2025-05-15 17:24:46 +0900

    게시판 데이터베이스 구축 및 게시판 메인 페이지 디자인

---
+++ BBS/src/main/java/bbs/Bbs.java
@@ -0,0 +1,49 @@
+package bbs;
+
+public class Bbs {
+	
+	private int bbsID;
+	private String bbstTitle;
+	private String userID;
+	private String bbsDate;
+	private String bbsContent;
+	private int bbsAvailable;
+	
+	public int getBbsID() {
+		return bbsID;
+	}
+	public void setBbsID(int bbsID) {
+		this.bbsID = bbsID;
+	}
+	public String getBbstTitle() {
+		return bbstTitle;
+	}
+	public void setBbstTitle(String bbstTitle) {
+		this.bbstTitle = bbstTitle;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getBbsDate() {
+		return bbsDate;
+	}
+	public void setBbsDate(String bbsDate) {
+		this.bbsDate = bbsDate;
+	}
+	public String getBbsContent() {
+		return bbsContent;
+	}
+	public void setBbsContent(String bbsContent) {
+		this.bbsContent = bbsContent;
+	}
+	public int getBbsAvailable() {
+		return bbsAvailable;
+	}
+	public void setBbsAvailable(int bbsAvailable) {
+		this.bbsAvailable = bbsAvailable;
+	}
+	
+}
